d9a54bddd1ab31afcd121e7622fc6dc67805f486
ScottThomasMiller <73297684+ScottThomasMiller@users.noreply.github.com>
2020-11-28 15:20:53 -0500

    Update run.py

--- nail/run.py
+++ nail/run.py
@@ -12,11 +12,9 @@ import os
 import sys
 import tempfile
 import torch
-import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
 import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
 import signal
 

1fd0ef1f21a0bbbcb4b4846b1a2ca8830d853764
Scott Miller <miller.scott.thomas@gmail.com>
2021-07-24 06:04:29 -0400

    initial Swift check in

---
+++ Swift/BCILab/BCILab/BCILabApp.swift
@@ -0,0 +1,17 @@
+//
+//  BCILabApp.swift
+//  BCILab
+//
+//  Created by Scott Miller on 7/24/21.
+//
+
+import SwiftUI
+
+@main
+struct BCILabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
